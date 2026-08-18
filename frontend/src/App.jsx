5a91394364981206da495c30efe84c9fd6339f59
MD-RIFAT-FAKIR <mdrifatfakir001@gmail.com>
2026-08-19 03:05:50 +0600

    ReactJS and reactBootstrap installation completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+
+  return (
+    <>
+      <button className='btn btn-primary' >button</button>
+    </>
+  )
+}
+
+export default App
